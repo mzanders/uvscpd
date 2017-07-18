37878b7c8e94b92418694032f0c24b8b4c193102
maarten <maarten@sof3.esterline.net>
2017-07-18 17:17:23 +0200

    add an option not to daemonize, might be usefull in the future

--- src/uvscpd.c
+++ src/uvscpd.c
@@ -17,6 +17,7 @@ void uvscpd_show_help(void);
 sig_atomic_t gsigterm_received = 0;
 sig_atomic_t gsighup_received = 0;
 sig_atomic_t gsigint_received = 0;
+int gDaemonize = 1;
 
 void signal_handler(int signal_number)
 {
@@ -45,11 +46,12 @@ int main(int argc, char *argv[])
   pid_t pid, sid;
   char * config_file = "/etc/uvscpd/uvscpd.cfg";
 
-  const char *const short_options = "hvc:";
+  const char *const short_options = "hvc:s";
   const struct option long_options[] = {// name, has_arg, flag, val
                                         {"help", 0, NULL, 'h'},
                                         {"version", 0, NULL, 'v'},
                                         {"config",required_argument, NULL, 'c'},
+                                        {"stay", 0, &gDaemonize, 0},
                                         {NULL, 0, NULL, 0}};
   struct sigaction sa;
 
@@ -58,6 +60,9 @@ int main(int argc, char *argv[])
   {
     switch (next_option)
     {
+      case 0:
+        break;
+        
       case 'h':
         uvscpd_show_help();
         exit(0);
@@ -90,40 +95,43 @@ int main(int argc, char *argv[])
   sigaction(SIGTERM, &sa, NULL);
   sigaction(SIGINT, &sa, NULL);
 
-  // Fork child
-  if (0 > (pid = fork()))
+  if (gDaemonize)
   {
-    fprintf(stderr, "Failed to fork.\n");
-    return -1;
+    // Fork child
+    if (0 > (pid = fork()))
+    {
+      fprintf(stderr, "Failed to fork.\n");
+      return -1;
+    }
+    else if (0 != pid)
+    {
+      exit(0); // Parent exits
+    }
+    sid = setsid(); // Become session leader
+    if (sid < 0)
+    {
+      // Failure
+      fprintf(stderr, "uvscpd: failed to become session leader.\n");
+      return -1;
+    }
+
+    umask(0); // Clear file mode creation mask
+
+    // Close the standard file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    syslog(LOG_INFO, "uvscpd started");
+
+    if (open("/", 0))
+    {
+      syslog(LOG_CRIT, "uvscpd: open / not 0: %m");
+    }
+
+    dup2(0, 1);
+    dup2(0, 2);
   }
-  else if (0 != pid)
-  {
-    exit(0); // Parent exits
-  }
-  sid = setsid(); // Become session leader
-  if (sid < 0)
-  {
-    // Failure
-    fprintf(stderr, "uvscpd: failed to become session leader.\n");
-    return -1;
-  }
-
-  umask(0); // Clear file mode creation mask
-
-  // Close the standard file descriptors
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-
-  syslog(LOG_INFO, "uvscpd started");
-
-  if (open("/", 0))
-  {
-    syslog(LOG_CRIT, "uvscpd: open / not 0: %m");
-  }
-
-  dup2(0, 1);
-  dup2(0, 2);
 
   controller(config_file);
 
