20ee978a6a492f74cddeb1d79330ebaad611926c
Maarten Zanders <maarten@localhost.localdomain>
2019-09-22 12:35:10 +0200

    Correct version.h

--- src/version.h
+++ src/version.h
@@ -17,8 +17,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSION_MAJOR       "0"
-#define VERSION_MINOR       "1"
+#define VERSION_MAJOR       "1"
+#define VERSION_MINOR       "0"
 #define VERSION_SUBMINOR    "0"
 #define VERSION_BUILD       "N/A"
 
